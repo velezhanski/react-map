129c181f1534a185757705c14ae1bce001f4fc64
Alex Filipenko <alexfilipenko719@gmail.com>
2020-09-09 18:04:11 +0300

    updated radius

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     });
 
   componentDidMount() {
-    fetch(`https://stg-services.benchapp.com/v1/free-agents?radius=10&longitude=${sampleLocation.lng}&latitude=${sampleLocation.lat}&sport=HOCKEY`)
+    fetch(`https://stg-services.benchapp.com/v1/free-agents?radius=3&longitude=${sampleLocation.lng}&latitude=${sampleLocation.lat}&sport=HOCKEY`)
       .then(response => {
         return response.json();
       })
